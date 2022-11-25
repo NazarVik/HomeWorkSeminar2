fd3d0bdc7678eb883729b0740df0c47493baf2da
NazarVik <Nazarov_dev@icloud.com>
2022-11-25 22:25:49 +0300

    finished task 15

--- task15/Program.cs
+++ task15/Program.cs
@@ -3,4 +3,9 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("введите  день недели от 1 до 7 = ");
+int day = int.Parse(Console.ReadLine());
+
+if ((day == 6) || (day == 7)) Console.WriteLine(day + " -> да");
+else Console.WriteLine(day + " -> нет");
